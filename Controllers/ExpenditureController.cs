9d78ef9329c120282c6cc84d2b134e1ff11b5900
Julien.Iz <69205008+IzyJu@users.noreply.github.com>
2020-12-07 11:30:23 +0100

    Add files via upload

---
+++ Controllers/ExpenditureController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TemplatingRazor.Models;
+
+namespace TemplatingRazor.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        private readonly ILogger<ExpenditureController> _logger;
+        public ExpenditureController(ILogger<ExpenditureController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Expenditure()
+        {           
+            List<Expenditure> Expends = new List<Expenditure>();
+            Expends.Add(new Expenditure
+            {
+                name = "Depense 1",
+                date = new DateTime(),
+                price = 10
+            });
+            Expends.Add(new Expenditure
+            {
+                name = "Depense 2",
+                date = new DateTime(),
+                price = 18
+            });
+            Expends.Add(new Expenditure
+            {
+                name = "Depense 3",
+                date = new DateTime(),
+                price = 50
+            }); 
+            Expends.Add(new Expenditure
+            {
+                name = "Depense 4",
+                date = new DateTime(),
+                price = 25
+            });
+            return View(Expends);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TemplatingRazor.Models;
+
+namespace TemplatingRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<String> names = new List<string>{ "Bernard", "Roger", "Jean-Paul" };
+            return View(names);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
